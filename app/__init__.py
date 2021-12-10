c14c0ed41dd0de40efb368b2bdaf38e4b6d88a35
Noakai Aronesty <noakai@gmail.com>
2021-12-10 09:16:33 -0500

    init files

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,14 @@
 # Hamsters of Destiny -- Annabel Zhang, Noakai Aronesty, Hebe Huang, Justin Zou
 # SoftDev
 # P01 -- The Hamster Wheet
+
+from os import urandom
+from flask import Flask
+
+app = Flask(__name__)
+app.secret_key = urandom(32)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
