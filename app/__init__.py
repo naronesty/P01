dfd8dec9e30c50e4fe294ae59cb2ca45b494ffca
AnnabelZhang1 <annabel.zhang1@gmail.com>
2021-12-11 18:40:23 -0500

    added tables

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,26 @@
 # Hamsters of Destiny -- Annabel Zhang, Noakai Aronesty, Hebe Huang, Justin Zou
 # SoftDev
 # P01 -- The Hamster Wheel
+# 2021-12-10
 
 from os import urandom
 from flask import Flask, request, redirect, render_template, session
 import urllib.request
 import json
+import sqlite3
 import random
+
+
+db = sqlite3.connect("hamster.db", check_same_thread=False)
+c = db.cursor()
+
+# creates users and profiles tables
+# pid is profile id
+c.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, pid TEXT)")
+# picture is profile pic; banner is profile banner
+c.execute("CREATE TABLE IF NOT EXISTS profiles(id INTEGER, username TEXT, picture TEXT, banner TEXT, biography TEXT, hobbies TEXT);")
+db.commit()
+
 
 app = Flask(__name__)
 app.secret_key = urandom(32)
@@ -25,4 +39,3 @@ def profile_generate():
 
 app.debug = True
 app.run()
-
