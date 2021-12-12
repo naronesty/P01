5000e5046006855f4d3b2d4315168b3b6ed6af22
Justin <justin82049@gmail.com>
2021-12-12 00:43:11 -0500

    moved the db to a separate file, used catFacts Api

--- app/__init__.py
+++ app/__init__.py
@@ -7,19 +7,7 @@ from os import urandom
 from flask import Flask, request, redirect, render_template, session
 import urllib.request
 import json
-import sqlite3
 import random
-
-
-db = sqlite3.connect("hamster.db", check_same_thread=False)
-c = db.cursor()
-
-# creates users and profiles tables
-# pid is profile id
-c.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, pid TEXT)")
-# picture is profile pic; banner is profile banner
-c.execute("CREATE TABLE IF NOT EXISTS profiles(id INTEGER, username TEXT, picture TEXT, banner TEXT, biography TEXT, hobbies TEXT);")
-db.commit()
 
 
 app = Flask(__name__)
@@ -29,11 +17,14 @@ app.secret_key = urandom(32)
 @app.route("/")
 def profile_generate():
     jokes = urllib.request.urlopen('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist')
-    d = json.loads(jokes.read())
-    if "joke" in d:
-        return d["joke"]
-    return d["setup"] + "<br>" + d["delivery"]
-
+    catFacts = urllib.request.urlopen('https://cat-fact.herokuapp.com/facts')
+    catList = json.loads(catFacts.read())
+    jokesDict = json.loads(jokes.read())
+    randomIndex = random.randrange(0,len(catList))
+    if "joke" in jokesDict:
+        return jokesDict["joke"]
+    
+    return jokesDict["setup"] + "<br>" + jokesDict["delivery"] + "<br>" + catList[randomIndex]['text']
 # def home():
 #     return "hello"
 
