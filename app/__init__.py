01cf04452e875fa9767f39792dd965a9b2618ac2
Hebe3834 <hebeluna00@gmail.com>
2021-12-13 19:37:05 -0500

    dropdowns lead to different results

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ def disp_home():
     return render_template('home.html')
 
 
-@app.route("/furrbook", methods=['GET', 'POST'])
+@app.route("/generate", methods=['GET', 'POST'])
 def profile_generate():
     jokes = urllib.request.urlopen('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist')
     catFacts = urllib.request.urlopen('https://cat-fact.herokuapp.com/facts')
@@ -34,8 +34,19 @@ def profile_generate():
         fullJoke = jokesDict["joke"]
     else:
         fullJoke = jokesDict["setup"] + "<br>" + jokesDict["delivery"]
-    return render_template('furrbook.html', joke=fullJoke, duckPic=duckDict['url'],
+    if request.method == 'POST': # determine which template to render
+        chosenTemp = request.form['templateMenu']
+        print(request.form['genreMenu']) # testing to see if we can access the genre user picked
+        if chosenTemp == "FurrbookChosen":
+            return render_template('furrbook.html', joke=fullJoke, duckPic=duckDict['url'],
                            catFact=catList[randomIndex]['text'])
+        elif chosenTemp == "DestinderChosen":
+            return render_template('destinder.html', joke=fullJoke, duckPic=duckDict['url'],
+                           catFact=catList[randomIndex]['text'])
+        elif chosenTemp == "HamstwitterChosen":
+            return render_template('hamstwitter.html', joke=fullJoke, duckPic=duckDict['url'],
+                           catFact=catList[randomIndex]['text'])
+    return render_template('home.html') # user did not select a template or something went wrong
 
 
 # def home():
