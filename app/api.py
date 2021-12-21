3550e4bbac502b72dd3da85d6f998fd24c1d9466
Noakai Aronesty <noakai@gmail.com>
2021-12-21 09:10:41 -0500

    cities

--- app/api.py
+++ app/api.py
@@ -106,6 +106,6 @@ def renderProfile(Filename, chosenGenre):
                            themePic=randomImg,
                            pfp=unsplash(chosenGenre),
                            adjective=adjective,
-                           animal=randomWordList('animal', 1)[0],
+                           animal=randomWordList('animal', 1)[0].capitalize(),
                            post1 = getMeme(), post2 = getMeme())
                         #    username = session['username']) #doesnt check if post1 and post2 are the same
