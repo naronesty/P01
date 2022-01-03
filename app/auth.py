0fcfd4c4abce1acad4d8890f129a71c08a2a4e02
AnnabelZhang1 <annabel.zhang1@gmail.com>
2022-01-03 09:23:23 -0500

    fixed viewing saved profile

--- app/auth.py
+++ app/auth.py
@@ -15,7 +15,7 @@ def create_db():
     c = db.cursor()
 
     c.execute("CREATE TABLE IF NOT EXISTS users (usernames TEXT, passwords TEXT);")
-    c.execute("CREATE TABLE IF NOT EXISTS profiles (pid INTEGER, name TEXT, username TEXT, template TEXT, pfp TEXT, banner TEXT, adjective TEXT, animal TEXT, joke TEXT, catFact TEXT, weatherFact TEXT, meme1 TEXT, meme2 TEXT, age TEXT, location TEXT, genre TEXT, other_genres TEXT, date TEXT, year TEXT);")
+    c.execute("CREATE TABLE IF NOT EXISTS profiles (pid INTEGER, name TEXT, username TEXT, template TEXT, pfp TEXT, banner TEXT, adjective TEXT, animal TEXT, joke TEXT, catFact TEXT, weatherFact TEXT, meme1 TEXT, meme2 TEXT, age TEXT, location TEXT, genre TEXT, date TEXT, year TEXT);")
     c.execute("CREATE TABLE IF NOT EXISTS favorites (users TEXT, id INTEGER);")
     db.close()
 
